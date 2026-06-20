2b6fd5f135a61f74a29564071a9fda4abbbae131
pdllerenas <pdllerenas@gmail.com>
2026-06-20 10:14:48 -0600

    Stats, Gamma Parameters, some constants

---
+++ src/common/Types.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <chrono>
+
+struct Stats {
+  double mean;
+  double variance;
+  double stddev;
+  double skewness;
+  double packet_separation_avg;
+};
+
+struct GammaParameters {
+  double rho;
+  double beta;
+  double shift;
+  double mean;
+};
+
+constexpr std::chrono::microseconds INTER_PACKET_SEPARATION(30000);  // 30 ms
+constexpr std::chrono::microseconds IPS_TOLERANCE = (INTER_PACKET_SEPARATION * 11) / 10;
+constexpr std::chrono::microseconds M_SEC_LOWERBOUND(10);
+
+constexpr size_t NUM_PACKETS = 5;
